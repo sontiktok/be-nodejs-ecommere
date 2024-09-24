07b6c4af95f2a450680a4ed5a8e32aefd558932c
sontiktok <sonvltiktok@gmail.com>
2024-09-24 11:40:55 +0700

    NODE 04. Setup env and config envirment for project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+require("dotenv").config;
+console.log("Port from .env:", process.env.PORT);
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const { default: helmet } = require("helmet");
 const compression = require("compression");
+
 // init middlewares
 app.use(morgan("dev"));
 app.use(helmet());
@@ -10,8 +13,8 @@ app.use(compression());
 
 // init db
 require("./dbs/init.mongodb");
-const { checkOverload } = require("./helpers/check.connect");
-checkOverload();
+// const { checkOverload } = require("./helpers/check.connect");
+// checkOverload();
 // init routes
 app.get("/", (req, res, next) => {
   return res.status(500).json({
--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const mongoose = require("mongoose");
-
+const {
+  db: { host, name, port },
+} = require("../configs/config.mongodb");
 const { countConnect } = require("../helpers/check.connect");
-const connectString = `mongodb://localhost:27017//shopDev`;
-
+const connectString = `mongodb://${host}:${port}//${name}`;
+console.log(`Connect string `, connectString);
 class Database {
   constructor() {
     this.connect();
